498e008ec40c1d96b8d4a3bd4c10a7e2700d0916
juana nakfour <jnakfour@ovpn-123-251.rdu2.redhat.com>
2017-09-26 09:31:00 -0500

    Redirecting 3-scale calls to avoid CORS

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -59,7 +59,7 @@ console.log("Starting queue")
   
 
 d3.queue()
-  .defer(d3.json, "http://localhost:8080/getstationstats")
+  .defer(d3.json, "http://python-analytics-myproject.173.230.141.17.xip.io/getstationstats")
   .defer(d3.json, "http://localhost:8080/getmobileosstats")
   .await(analyze);
 
