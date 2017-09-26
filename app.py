4bc1834d47ad5747bff59106c8694267a2f6af28
juana nakfour <jnakfour@ovpn-123-251.rdu2.redhat.com>
2017-09-26 13:30:43 -0500

    Grouping calls to load database in functions

--- app.py
+++ app.py
@@ -50,66 +50,53 @@ print("Start")
 spark = SparkSession.builder.appName("mobileanalytics").config("spark.mongodb.input.uri", "mongodb://admin:admin@mongodb/sampledb.bikerental").config("spark.mongodb.output.uri", "mongodb://admin:admin@mongodb/sampledb.bikerental").getOrCreate()
 #local
 #spark = SparkSession.builder.master("local").appName("mobileanalytics").config("spark.driver.bindAddress", "127.0.0.1").config("spark.mongodb.input.uri", "mongodb://127.0.0.1/sampledb.bikerental").config("spark.mongodb.output.uri", "mongodb://127.0.0.1/sampledb.bikerental").getOrCreate()
-
-
-print("Started Spark")
-df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
-print("posting df")
-df.printSchema()
-print(df.show())
-
-
-#Convert starttime from string to timestamp
-#'yyyy-MM-dd HH:mm:ss'
-typedbikerentaldf= df.select(unix_timestamp(df.starttime).cast('timestamp').alias('starttimehour'),\
-    'startstationid')\
-    .cache()
-# we only need the hour to put rentals in buckets of morning, lunch, afternoon, evening by station id
-typedbikerentaldfhour= typedbikerentaldf.select(hour('starttimehour').alias('starttimehour'),\
-    'startstationid')\
-    .cache()
-print(typedbikerentaldfhour.show())
-typedbikerentaldfhour.printSchema()
-
-#create a function to return the integer value of the time of day
-func_udf = udf(assign_tod, StringType())
-dfbuckets = typedbikerentaldfhour.withColumn('daypart',func_udf(typedbikerentaldfhour['starttimehour']))
-
-df3=dfbuckets.groupBy("daypart", "startstationid").agg(count("*"))
-df3=df3.withColumn("startstationid", dfbuckets["startstationid"].cast("integer"))
-df3 = df3.select(col("daypart").alias("daypart"),col("startstationid").alias("startstationid"),col("count(1)").alias("rentalcount"))
-
-print(df3.show())
-df3.printSchema()
-print("Number of rows")
-#print(df3.count())
-
-#group by daypart
-#df3=df3.groupBy("daypart").agg(count("*"))
-#print(df3.show())
-#df3.printSchema()
-#print("Number of rows")
-df3=df3.orderBy(df3.startstationid.desc())
-
-print(df3.show())
-df3.printSchema()
 #spark.stop()
 
-###################### Getting mobileos stats #######################
 
-
-mobiledata = df.select(col("mobileos").alias("mobileos"))
-mobiledata=mobiledata.groupBy("mobileos").agg(count("*"))
-mobiledata = mobiledata.select(col("mobileos").alias("mobileos"),col("count(1)").alias("mobileoscount"))
-print (mobiledata.show())
     
-######################
-#mongoClient = MongoClient('mongodb://admin:admin@mongodb')
-#db= mongoClient.sampledb
-#collection=db.bikerentalmembership
-#for rental in collection.find():
- #   pprint.pprint(rental)
+def getDayStats():
+    print("Getting Day stats")
+    df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+    print("posting df")
+    df.printSchema()
+    print(df.show())
+    #Convert starttime from string to timestamp
+    #'yyyy-MM-dd HH:mm:ss'
+    typedbikerentaldf= df.select(unix_timestamp(df.starttime).cast('timestamp').alias('starttimehour'),\
+    'startstationid')\
+    .cache()
+    # we only need the hour to put rentals in buckets of morning, lunch, afternoon, evening by station id
+    typedbikerentaldfhour= typedbikerentaldf.select(hour('starttimehour').alias('starttimehour'),\
+    'startstationid')\
+    .cache()
+    print(typedbikerentaldfhour.show())
+    typedbikerentaldfhour.printSchema()
 
+    #create a function to return the string value of the time of day
+    func_udf = udf(assign_tod, StringType())
+    dfbuckets = typedbikerentaldfhour.withColumn('daypart',func_udf(typedbikerentaldfhour['starttimehour']))
+    print("Serving Station Stats data")
+    test=dfbuckets.groupBy("daypart").count()
+    print("Printing group by daypart")
+    print(test.show())
+    resultlist=test.toJSON().collect()
+    return resultlist
+    
+def getMobielOsStats():
+    print("Getting Day stats")
+    df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+    print("posting df")
+    df.printSchema()
+    print(df.show())
+    ###################### Getting mobileos stats #######################
+    mobiledata = df.select(col("mobileos").alias("mobileos"))
+    mobiledata=mobiledata.groupBy("mobileos").agg(count("*"))
+    mobiledata = mobiledata.select(col("mobileos").alias("mobileos"),col("count(1)").alias("mobileoscount"))
+    print (mobiledata.show())
+    resultlist=mobiledata.toJSON().collect()
+    print(resultlist)
+    return resultlist
+    
 ################### app Web Server #####################
 app = Flask(__name__)
 #CORS(app)
@@ -122,28 +109,18 @@ def mainRoute():
 
 @app.route("/getstationstats")
 def dataRoute():
-    print("Serving Station Stats data")
-    ########################### Testing toJOSON ###############
-    #test=fullbikerentaldf.limit(1)
-    test=dfbuckets.groupBy("daypart").count()
-    print("Printing group by daypart")
-    print(test.show())
-    resultlist=test.toJSON().collect()
-    print(resultlist)
-    json_results = json.dumps(resultlist)
-    #json_results = json.dumps(test, default=json_util.default)
+    results=getDayStats()
+    print(results)
+    json_results = json.dumps(results)
     print(json_results)
     return json_results
 
 @app.route("/getmobileosstats")
 def mobiledataRoute():
     print("Serving Mobile OS")
-    ########################### Testing toJOSON ###############
-    #test=fullbikerentaldf.limit(1)
-    resultlist=mobiledata.toJSON().collect()
-    print(resultlist)
-    json_results = json.dumps(resultlist)
-    #json_results = json.dumps(test, default=json_util.default)
+    results=getMobileOsStats()
+    print(results)
+    json_results = json.dumps(results)
     print(json_results)
     return json_results
     
