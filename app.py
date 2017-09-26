0731de90961dc1abcf59af543d30e52a29757102
juana nakfour <jnakfour@jnakfour-Red-Hat-Username-OSX.local>
2017-09-25 20:52:42 -0500

    Adding headers for cors

--- app.py
+++ app.py
@@ -153,7 +153,14 @@ def trainRoute():
     # run learning on apache spark
     
     return null
-  
+
+####### Allowing access-control
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
 
 
     
