b52a1702eafe305989b895d82869eed154dada0e
juana nakfour <jnakfour@jnakfour-Red-Hat-Username-OSX.local>
2017-09-26 21:10:15 -0500

    Creating a dashboard

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -115,6 +115,10 @@ function analyze(error, stationdata, mobiledata, scaledata, poststartrental, pos
     
     var chart2 = c3.generate({
         bindto: '#mobileosdatachart',
+        size: {
+        height: 115,
+        width: 360
+        },
         data: {
             //json: mobiledata,
             json: [count],
